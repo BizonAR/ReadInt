836d71919ed7e7ea23724383db6fc4c11d0f7e49
mikaa2001 <mikaa2001@gmail.com>
2024-02-28 13:16:58 +0500

    ReadInt

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ReadInt
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int number;
+			number = ReadInt(out number);
+			Console.WriteLine("Вы ввели число: " + number);
+		}
+		static int ReadInt(out int number)
+		{
+			do
+			{
+				Console.Write("Введите число: ");
+			} while (int.TryParse(Console.ReadLine(), out number) == false);
+			return number;
+		}
+	}
+}
